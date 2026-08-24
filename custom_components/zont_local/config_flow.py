15f27c799a4cdf2c901ff57331a0f3a35ec3a4a4
Nikolay <110602332+NikaSir@users.noreply.github.com>
2026-08-24 09:24:54 +0300

    Add ZONT Local config flow

---
+++ custom_components/zont_local/config_flow.py
@@ -0,0 +1,25 @@
+"""Config flow for ZONT Local & UI."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant import config_entries
+
+from .const import DOMAIN, NAME
+
+
+class ZontLocalConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Configure ZONT Local & UI."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input: dict[str, Any] | None = None):
+        """Create the single UI host entry."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is not None:
+            return self.async_create_entry(title=NAME, data={})
+
+        return self.async_show_form(step_id="user")
